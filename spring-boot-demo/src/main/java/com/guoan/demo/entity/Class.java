1e8e9314256ec8780f49f42f00bff03b9f31c8f4
fanpeng1100 <343405399@qq.com>
2016-08-31 18:25:28 +0800

    spring-boot-demo

---
+++ spring-boot-demo/src/main/java/com/guoan/demo/entity/Class.java
@@ -0,0 +1,43 @@
+package com.guoan.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.sun.istack.internal.NotNull;
+
+/**
+ * 班级类
+ * @author fan
+ *
+ */
+
+public class Class {
+
+	private long id;	//班级id
+	private String name;//班级名称
+	private Boolean isAvailable;//是否可用
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Boolean getIsAvailable() {
+		return isAvailable;
+	}
+	public void setIsAvailable(Boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+	
+} 
